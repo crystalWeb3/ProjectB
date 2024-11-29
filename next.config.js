5f57b2c35547f29f7ad5914430cfd0fdfccf7351
crystalWeb3 <crystal.apolon@gmail.com>
2024-11-29 10:43:33 +0100

    update_next_version

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/',
-            destination: '/main/home',
-            permanent: true,
-          },
-        ]
-      },
+   
 }
 
 module.exports = nextConfig
